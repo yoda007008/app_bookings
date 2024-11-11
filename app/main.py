f685e3d029e084de4ba8adf5f335db11264dfed6
yoda007008 <masteryoda01010101@gmail.com>
2024-11-11 19:38:49 +0300

    fixing main.py endpoint

--- app/main.py
+++ app/main.py
@@ -10,19 +10,6 @@ app = FastAPI()
 app.include_router(router_users)
 app.include_router(router_bookings)
 
-@app.get("/hotels/{hotel_id}")
-async def hotels(
-    location: str,
-    hotel_id: int, 
-    date_to: date, 
-    date_from,
-    has_spa: Optional[bool] = None,
-    stars: Optional[int] = Query(None, ge=1, le=5),
-):
-    return date_to, date_from, location
-
-
-
 
 # запуск сервера (команда) uvicorn app.main:app --reload
 
