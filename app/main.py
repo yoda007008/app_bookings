d3237d4b2fe6902bd21d1185a86e5e76ce06b0d2
yoda007008 <masteryoda01010101@gmail.com>
2024-11-11 19:47:27 +0300

    new router in pages

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from datetime import date
 from pydantic import BaseModel
 from app.booking.router import router as router_bookings
 from app.users.router import router as router_users
-
+from app.pages.router import router as router_pages
 app = FastAPI()
 
 app.include_router(router_users)
 app.include_router(router_bookings)
-
+app.include_router(router_pages)
 
 # запуск сервера (команда) uvicorn app.main:app --reload
 
