a875b9623765d3bc804a753fbd893b050918adde
yoda007008 <masteryoda01010101@gmail.com>
2024-11-13 11:23:25 +0300

    celery for images example

---
+++ app/images/router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, UploadFile
+import shutil
+
+from app.tasks.tasks import process_pic
+
+
+router = APIRouter( 
+    prefix="/images",
+    tags=["Загрузка картинок"]
+)
+
+@router.post("/hotels")
+async def add_hotel_image(name: int, file: UploadFile):
+    im_path = f"app/static/{name}.webp"
+    with open(im_path, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    process_pic.delay(im_path)
--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -3,5 +3,5 @@ from celery import Celery
 celery = Celery(
     "tasks",
     broker="redis://localhost:6379",
-    tasks=["app.tasks.tasks"]
+    include=["app.tasks.tasks"]
 )
