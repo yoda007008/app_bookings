176222d2edeb06ebf41daddb5ecdb21c693ffecb
yoda007008 <masteryoda01010101@gmail.com>
2024-11-15 11:28:45 +0300

    new admin panel for users SQLAdmin

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ from fastapi import FastAPI, Query
 from datetime import date
 from pydantic import BaseModel
 from app.booking.router import router as router_bookings
+from app.users.models import Users
 from app.users.router import router as router_users
 from app.pages.router import router as router_pages
+from sqladmin import Admin, ModelView
 
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
-
+from app.database import engine
 from redis import asyncio as aioredis
 
 app = FastAPI()
@@ -24,6 +26,20 @@ def startup():
     redis = aioredis.from_url("redis://localhost:6379", encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="cache")
 
+
+app = FastAPI()
+admin = Admin(app, engine)
+
+
+class UsersAdmin(ModelView, model=Users):
+    column_list = [Users.id, Users.email]
+    can_delete = False
+    name = "Пользователь"
+    name_plural = "Пользователи"
+    icon = "fa-solid fa-user"
+    
+
+admin.add_view(UsersAdmin)
 
 # запуск сервера (команда) uvicorn app.main:app --reload
 
